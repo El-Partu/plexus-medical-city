60279e22434d534456c0b36b6fe16b11fb835592
El-Partu <laurentpartu@gmail.com>
2025-11-19 08:16:01 +0000

    updated main layout header

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Plexus Medical City') }}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net" rel="preconnect">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Karantina&display=swap" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net" rel="preconnect">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Karantina&display=swap" rel="stylesheet">
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/4b12d28613.js" crossorigin="anonymous"></script>
+    <!-- External CSS / JS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/4b12d28613.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/echarts@6.0.0/dist/echarts.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-        {{-- <script defer src="httpscdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
-        <script src="https://cdn.jsdelivr.net/npm/echarts@6.0.0/dist/echarts.min.js"></script>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script nomodule src="js/countUp.umd.js"></script>
-        <script nomodule src="js/main-for-legacy.js"></script>
+    <!-- Initialize AOS -->
+    <script>
+        AOS.init();
+    </script>
 
-        <script>
-            AOS.init();
-        </script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
+    <!-- Laravel Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
     </head>
 
     <body class="font-sans">
